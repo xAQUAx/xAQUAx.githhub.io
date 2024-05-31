e47ffecfeb1613d37975cd469d4d4e953e11721c
xAQUAx <mix130ort22@gmail.com>
2024-05-31 10:06:04 +0300

    second commit

---
+++ project2/labs-master/labs-master/js/main.js
@@ -0,0 +1,60 @@
+
+	var trigger=0, name, age, sex, education, mathematic, asu, cutting, programming; // обьявляем переменные которые понадобятся в ходе работы
+		document.getElementById("startTest").addEventListener("click",test) // добавляем слушателя события click к элементу с id startTest
+		function test(){ // Функция которая опрашивает пользователя
+			name = prompt("Введите имя");
+			age = prompt("Введите возраст");
+			sex = prompt("Введите пол");
+			education = confirm("У вас есть высше образование?");
+			mathematic = confirm("Вы знаете математику?");
+			asu = confirm("Вы знаете АСУ ТП?");
+			cutting = confirm("Вы знаете теорию резания?");
+			programming = confirm("Умеете программировать?");
+			writeToPage();
+		}
+		function writeToPage(){ // функция которая записывает значение из наших переменных в элементы на странице
+			/*
+			document.getElementById("name") - возвращает элемент с id name
+			document.getElementById("name").innerText - текст этого элемента
+			У ЧЕКБОКСОВ НЕТ ТЕКСТА, поэтому мы пишем в свойство checked значения true или false
+			document.getElementById("education").checked = education;
+			*/
+			$("#name").val(name)
+			$("#age").val(age)
+			$("#sex").val(sex)
+			$("#sex").focus();
+			$("#age").focus();
+			$("#name").focus();
+			document.getElementById("education").checked = education;
+			document.getElementById("mathematic").checked = mathematic;
+			document.getElementById("asu").checked = asu;
+			document.getElementById("cutting").checked = cutting;
+			document.getElementById("programming").checked = programming;
+			trigger=1;
+			document.getElementById("meTake").style.display="block";
+			myFunction();
+		}
+		x.style.display = "none";
+		function myFunction() { // эта функция автоматически проверяет подходит ли наш кандидат: "кандидаты со знанем АСУ ТП и высшим
+					//образованием".
+			document.getElementById("startTest").style.display="none";
+			document.getElementById("education").disabled=true;
+			document.getElementById("mathematic").disabled=true;
+			document.getElementById("asu").disabled=true;
+			document.getElementById("cutting").disabled=true;
+			document.getElementById("programming").disabled=true;
+	    	var x = document.getElementById("div1");
+			
+		if (trigger==1) {
+		if (document.getElementById("mathematic").checked ==true && document.getElementById("programming").checked ==true)
+		{
+       		var text ='<p class="stroke">ВЫ НАМ ПОДОШЛИ!</p>'
+		x.innerHTML = text;
+		x.style.display = "block";
+		}
+		else 	{
+        	var text ='<p class="stroke">ВЫ НАМ НЕ НУЖНЫ!</p>'
+		x.innerHTML = text;
+			}
+		}
+		}
